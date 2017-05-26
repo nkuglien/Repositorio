ee59bb8266844751b6b0538cf6bec81c9a2ca372
nico_ <nico_@DESKTOP-UI6V6UM>
2017-05-26 02:17:18 -0300

    Prendaremote

---
+++ src/RemoteObject/PrendaRemote.java
@@ -0,0 +1,23 @@
+package RemoteObject;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import DTO.PrendaDTO;
+
+public interface PrendaRemote extends Remote {
+
+	
+	public boolean verificarPrenda(int parseInt) throws RemoteException;
+
+	public void altaPrenda(int parseInt, String text)throws RemoteException;
+
+	public void bajaprenda(int parseInt)throws RemoteException;
+
+
+	public PrendaDTO solicitarPrendaView(int parseInt) throws RemoteException;
+
+
+	public void modificarPrenda(PrendaDTO pv, int parseInt) throws RemoteException;
+
+}
