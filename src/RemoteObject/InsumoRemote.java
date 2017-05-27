298b84b8fceea8107ee8139edf6d947ed8c564f3
Ivan Addolorato <Coco@Mac-mini-de-Ivan.local>
2017-05-27 20:20:29 -0300

    alta insumo

--- src/RemoteObject/InsumoRemote.java
+++ src/RemoteObject/InsumoRemote.java
@@ -7,15 +7,13 @@ import DTO.InsumoDTO;
 
 public interface InsumoRemote extends Remote {
 
-	
-	public boolean verificarInsumo(int parseInt) throws RemoteException;
-
-	public void altaInsumo(int parseInt, String text, String text2, int parseInt2, int parseInt3) throws RemoteException;
+	public InsumoDTO altaInsumo(InsumoDTO insumo) throws RemoteException;
 
 	public void bajaInsumo(int parseInt) throws RemoteException;
 
-	public InsumoDTO solicitarInsumoView(int parseInt)  throws RemoteException;
-
+	public InsumoDTO solicitarInsumoView(int parseInt) throws RemoteException;
 
 	public void modificarInsumo(InsumoDTO iv, int parseInt) throws RemoteException;
+
+	public InsumoDTO buscarInsumo(Long codigo) throws RemoteException;
 }
