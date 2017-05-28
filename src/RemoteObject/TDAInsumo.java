136ef53e2758cc6620bc9711275f36aa22fc14f9
Ivan Addolorato <Coco@Mac-mini-de-Ivan.local>
2017-05-28 10:12:46 -0300

    arreglos por el cambios de los tda

--- src/RemoteObject/TDAInsumo.java
+++ src/RemoteObject/TDAInsumo.java
@@ -7,15 +7,13 @@ import DTO.InsumoDTO;
 
 public interface TDAInsumo extends Remote {
 
-	
-	public boolean verificarInsumo(int parseInt) throws RemoteException;
+	public InsumoDTO buscarInsumo(Long codigo) throws RemoteException;
 
-	public void altaInsumo(int parseInt, String text, String text2, int parseInt2, int parseInt3) throws RemoteException;
+	public InsumoDTO altaInsumo(InsumoDTO dto) throws RemoteException;
 
-	public void bajaInsumo(int parseInt) throws RemoteException;
+	public InsumoDTO bajaInsumo(Long codigo) throws RemoteException;
 
-	public InsumoDTO solicitarInsumoView(int parseInt)  throws RemoteException;
-
+	public InsumoDTO solicitarInsumoView(int parseInt) throws RemoteException;
 
 	public void modificarInsumo(InsumoDTO iv, int parseInt) throws RemoteException;
 }
