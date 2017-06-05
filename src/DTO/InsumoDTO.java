a9ef8a19824d3d990bf19534a3dfa3898b427a71
ivancoco123 <elcoco89@gmail.com>
2017-06-05 20:47:56 -0300

    Update InsumoDTO.java

--- src/DTO/InsumoDTO.java
+++ src/DTO/InsumoDTO.java
@@ -9,7 +9,7 @@ public class InsumoDTO implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 701547294209629418L;
-	private long id;
+	private Long id;
 	private Long codigo;
 	private String descripcion;
 	private String nombre;
@@ -57,11 +57,11 @@ public class InsumoDTO implements Serializable {
 		this.cantCompra = cantCompra;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	
