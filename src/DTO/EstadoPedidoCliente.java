fac78f1978b0f0095ef0170ef6e8d01eed610bff
Ezequiel Maranga <ezequiel.maranga@gmail.com>
2017-07-02 10:04:26 -0300

    estado

--- src/DTO/EstadoPedidoCliente.java
+++ src/DTO/EstadoPedidoCliente.java
@@ -1,10 +1,14 @@
 package DTO;
 
-public enum EstadoPedidoCliente {
+import java.io.Serializable;
+
+public enum EstadoPedidoCliente implements Serializable {
+	
 	CREADO, 	// por el cliente
 	VALIDADO,	// por sucursal
 	RECHAZADO,  // por sucursal
 	ACEPTADO, 	// por el cliente
 	CANCELADO,  // por el cliente
 	COMPLETO 	// listo para despacho
+
 }
